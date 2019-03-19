536ee27f655c23e6b0a9630f11d23371a286a5e4
Santosh Potu <santosh-potu@users.noreply.github.com>
2019-03-19 16:01:10 +0530

    date picker jquery validations

--- php_code/jquery_form_validation.php
+++ php_code/jquery_form_validation.php
@@ -11,7 +11,6 @@
     <title>
      JQuery Form validation Demo
     </title>   
-        
 	        <style>
          * { font-family: Verdana; font-size: 96%; }
         label { width: 10em; float: left; }
@@ -20,10 +19,12 @@
     .submit { margin-left: 12em; }
     em { font-weight: bold; padding-right: 1em; vertical-align: top; }
         </style>
-    <script type="text/javascript" src="scripts/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="scripts/jquery.validate.min.js"></script>
-    <link rel="stylesheet" href="scripts/calen/jquery.datepick.css" />
-    <script type="text/javascript" src="scripts/calen/jquery.datepick.js"></script>
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/datepicker_style.css">
+        <script src="scripts/jquery-1.12.4.js"></script>
+        <script src="scripts/jquery-ui.js"></script>
+        <script type="text/javascript" src="scripts/jquery.validate.min.js"></script>
+    
     <script type="text/javascript">
         $(document).ready(function(){
             
@@ -68,10 +69,12 @@
                 }
             });
   
-      $('#doj').datepick();
-      $('#doj').datepick({dateFormat: 'dd/mm/yyyy'});  
-      $('#dob').datepick();
-      $('#dob').datepick({dateFormat: 'dd/mm/yyyy'});
+      $('#doj').datepicker({
+          dateFormat: "dd-mm-yy"
+      });  
+      $('#dob').datepicker({
+          dateFormat: "dd-mm-yy"
+      });
   });
       
     </script>
@@ -96,15 +99,15 @@
                 </tr>
                 <tr>
                     <td><label for="pwd">Password</label></td>
-                    <td><input type="password" id="pwd" name="pwd" value="" /></td>
+                    <td><input type="password" autocomplete="off" id="pwd" name="pwd" value="" /></td>
                 </tr>
                 <tr>
                     <td><label for="confirm_pwd">Confirm Password</label></td>
-                    <td><input type="password" id="confirm_pwd" name="confirm_pwd" value="" /></td>
+                    <td><input type="password" autocomplete="off" id="confirm_pwd" name="confirm_pwd" value="" /></td>
                 </tr>
                 <tr>
                     <td><label for="email_id">Email</label></td>
-                    <td><input type="text" id="email_d" name="email_id" value="" />
+                    <td><input type="text" autocomplete="off" id="email_d" name="email_id" value="" />
                 </tr>
                 <tr>
                     <td><label for="mobile_number">Mobile</label></td>
