b1f576f700e649f59fef0b33a770e0ffa530f324
Santosh <santoshreddy.potu@gmail.com>
2019-06-06 15:36:19 +0530

    Db chnages

--- php_code/sql/php_tutorial.sql
+++ php_code/sql/php_tutorial.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 02, 2018 at 03:11 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.0.21
+-- Host: localhost:3306
+-- Generation Time: Jun 06, 2019 at 03:34 PM
+-- Server version: 5.7.26-0ubuntu0.18.10.1
+-- PHP Version: 7.2.17-0ubuntu0.18.10.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -40,180 +38,7 @@ CREATE TABLE `cart_products` (
 
 INSERT INTO `cart_products` (`product_id`, `product_name`, `product_price`) VALUES
 (1, 'Nokia 1100', 2800),
-(2, 'Iphone 6', 66000),
-(3, 'Mi Fitband', 1200),
-(4, 'Vega Helmet', 1600),
-(5, 'Redmi Note 4', 10000),
-(6, 'Micromax Bharat 1', 2500);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `currencies`
---
-
-CREATE TABLE `currencies` (
-  `id` int(11) NOT NULL,
-  `code` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `currencies`
---
-
-INSERT INTO `currencies` (`id`, `code`, `description`) VALUES
-(1, 'AFA', 'Afghanistan Afghani'),
-(2, 'ALL', 'Albanian Lek'),
-(3, 'DZD', 'Algerian Dinar'),
-(4, 'ARS', 'Argentine Peso'),
-(5, 'AWG', 'Aruba Florin'),
-(6, 'AUD', 'Australian Dollar'),
-(7, 'BSD', 'Bahamian Dollar'),
-(8, 'BHD', 'Bahraini Dinar'),
-(9, 'BDT', 'Bangladesh Taka'),
-(10, 'BBD', 'Barbados Dollar'),
-(11, 'BZD', 'Belize Dollar'),
-(12, 'BMD', 'Bermuda Dollar'),
-(13, 'BTN', 'Bhutan Ngultrum'),
-(14, 'BOB', 'Bolivian Boliviano'),
-(15, 'BWP', 'Botswana Pula'),
-(16, 'BRL', 'Brazilian Real'),
-(17, 'GBP', 'British Pound'),
-(18, 'BND', 'Brunei Dollar'),
-(19, 'BIF', 'Burundi Franc'),
-(20, 'XOF', 'CFA Franc (BCEAO)'),
-(21, 'XAF', 'CFA Franc (BEAC)'),
-(22, 'KHR', 'Cambodia Riel'),
-(23, 'CAD', 'Canadian Dollar'),
-(24, 'CVE', 'Cape Verde Escudo'),
-(25, 'KYD', 'Cayman Islands Dollar'),
-(26, 'CLP', 'Chilean Peso'),
-(27, 'CNY', 'Chinese Yuan'),
-(28, 'COP', 'Colombian Peso'),
-(29, 'KMF', 'Comoros Franc'),
-(30, 'CRC', 'Costa Rica Colon'),
-(31, 'HRK', 'Croatian Kuna'),
-(32, 'CUP', 'Cuban Peso'),
-(33, 'CYP', 'Cyprus Pound'),
-(34, 'CZK', 'Czech Koruna'),
-(35, 'DKK', 'Danish Krone'),
-(36, 'DJF', 'Dijibouti Franc'),
-(37, 'DOP', 'Dominican Peso'),
-(38, 'XCD', 'East Caribbean Dollar'),
-(39, 'EGP', 'Egyptian Pound'),
-(40, 'SVC', 'El Salvador Colon'),
-(41, 'EEK', 'Estonian Kroon'),
-(42, 'ETB', 'Ethiopian Birr'),
-(43, 'EUR', 'Euro'),
-(44, 'FKP', 'Falkland Islands Pound'),
-(45, 'GMD', 'Gambian Dalasi'),
-(46, 'GHC', 'Ghanian Cedi'),
-(47, 'GIP', 'Gibraltar Pound'),
-(48, 'XAU', 'Gold Ounces'),
-(49, 'GTQ', 'Guatemala Quetzal'),
-(50, 'GNF', 'Guinea Franc'),
-(51, 'GYD', 'Guyana Dollar'),
-(52, 'HTG', 'Haiti Gourde'),
-(53, 'HNL', 'Honduras Lempira'),
-(54, 'HKD', 'Hong Kong Dollar'),
-(55, 'HUF', 'Hungarian Forint'),
-(56, 'ISK', 'Iceland Krona'),
-(57, 'INR', 'Indian Rupee'),
-(58, 'IDR', 'Indonesian Rupiah'),
-(59, 'IQD', 'Iraqi Dinar'),
-(60, 'ILS', 'Israeli Shekel'),
-(61, 'JMD', 'Jamaican Dollar'),
-(62, 'JPY', 'Japanese Yen'),
-(63, 'JOD', 'Jordanian Dinar'),
-(64, 'KZT', 'Kazakhstan Tenge'),
-(65, 'KES', 'Kenyan Shilling'),
-(66, 'KRW', 'Korean Won'),
-(67, 'KWD', 'Kuwaiti Dinar'),
-(68, 'LAK', 'Lao Kip'),
-(69, 'LVL', 'Latvian Lat'),
-(70, 'LBP', 'Lebanese Pound'),
-(71, 'LSL', 'Lesotho Loti'),
-(72, 'LRD', 'Liberian Dollar'),
-(73, 'LYD', 'Libyan Dinar'),
-(74, 'LTL', 'Lithuanian Lita'),
-(75, 'MOP', 'Macau Pataca'),
-(76, 'MKD', 'Macedonian Denar'),
-(77, 'MGF', 'Malagasy Franc'),
-(78, 'MWK', 'Malawi Kwacha'),
-(79, 'MYR', 'Malaysian Ringgit'),
-(80, 'MVR', 'Maldives Rufiyaa'),
-(81, 'MTL', 'Maltese Lira'),
-(82, 'MRO', 'Mauritania Ougulya'),
-(83, 'MUR', 'Mauritius Rupee'),
-(84, 'MXN', 'Mexican Peso'),
-(85, 'MDL', 'Moldovan Leu'),
-(86, 'MNT', 'Mongolian Tugrik'),
-(87, 'MAD', 'Moroccan Dirham'),
-(88, 'MZM', 'Mozambique Metical'),
-(89, 'MMK', 'Myanmar Kyat'),
-(90, 'NAD', 'Namibian Dollar'),
-(91, 'NPR', 'Nepalese Rupee'),
-(92, 'ANG', 'Neth Antilles Guilder'),
-(93, 'NZD', 'New Zealand Dollar'),
-(94, 'NIO', 'Nicaragua Cordoba'),
-(95, 'NGN', 'Nigerian Naira'),
-(96, 'KPW', 'North Korean Won'),
-(97, 'NOK', 'Norwegian Krone'),
-(98, 'OMR', 'Omani Rial'),
-(99, 'XPF', 'Pacific Franc'),
-(100, 'PKR', 'Pakistani Rupee'),
-(101, 'XPD', 'Palladium Ounces'),
-(102, 'PAB', 'Panama Balboa'),
-(103, 'PGK', 'Papua New Guinea Kina'),
-(104, 'PYG', 'Paraguayan Guarani'),
-(105, 'PEN', 'Peruvian Nuevo Sol'),
-(106, 'PHP', 'Philippine Peso'),
-(107, 'XPT', 'Platinum Ounces'),
-(108, 'PLN', 'Polish Zloty'),
-(109, 'QAR', 'Qatar Rial'),
-(110, 'ROL', 'Romanian Leu'),
-(111, 'RUB', 'Russian Rouble'),
-(112, 'WST', 'Samoa Tala'),
-(113, 'STD', 'Sao Tome Dobra'),
-(114, 'SAR', 'Saudi Arabian Riyal'),
-(115, 'SCR', 'Seychelles Rupee'),
-(116, 'SLL', 'Sierra Leone Leone'),
-(117, 'XAG', 'Silver Ounces'),
-(118, 'SGD', 'Singapore Dollar'),
-(119, 'SKK', 'Slovak Koruna'),
-(120, 'SIT', 'Slovenian Tolar'),
-(121, 'SBD', 'Solomon Islands Dollar'),
-(122, 'SOS', 'Somali Shilling'),
-(123, 'ZAR', 'South African Rand'),
-(124, 'LKR', 'Sri Lanka Rupee'),
-(125, 'SHP', 'St Helena Pound'),
-(126, 'SDD', 'Sudanese Dinar'),
-(127, 'SRG', 'Surinam Guilder'),
-(128, 'SZL', 'Swaziland Lilageni'),
-(129, 'SEK', 'Swedish Krona'),
-(130, 'TRY', 'Turkey Lira'),
-(131, 'CHF', 'Swiss Franc'),
-(132, 'SYP', 'Syrian Pound'),
-(133, 'TWD', 'Taiwan Dollar'),
-(134, 'TZS', 'Tanzanian Shilling'),
-(135, 'THB', 'Thai Baht'),
-(136, 'TOP', 'Tonga Pa\'anga'),
-(137, 'TTD', 'Trinidad&amp;Tobago Dollar'),
-(138, 'TND', 'Tunisian Dinar'),
-(139, 'TRL', 'Turkish Lira'),
-(140, 'USD', 'U.S. Dollar'),
-(141, 'AED', 'UAE Dirham'),
-(142, 'UGX', 'Ugandan Shilling'),
-(143, 'UAH', 'Ukraine Hryvnia'),
-(144, 'UYU', 'Uruguayan New Peso'),
-(145, 'VUV', 'Vanuatu Vatu'),
-(146, 'VEB', 'Venezuelan Bolivar'),
-(147, 'VND', 'Vietnam Dong'),
-(148, 'YER', 'Yemen Riyal'),
-(149, 'YUM', 'Yugoslav Dinar'),
-(150, 'ZMK', 'Zambian Kwacha'),
-(151, 'ZWD', 'Zimbabwe Dollar');
+(2, 'Iphone 6', 66000);
 
 -- --------------------------------------------------------
 
@@ -222,8 +47,8 @@ INSERT INTO `currencies` (`id`, `code`, `description`) VALUES
 --
 
 CREATE TABLE `ip_hits` (
-  `record_id` int(11) NOT NULL,
-  `user_agent` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `user_agent` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `session_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `hits` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `ip_address` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
@@ -234,9 +59,8 @@ CREATE TABLE `ip_hits` (
 -- Dumping data for table `ip_hits`
 --
 
-INSERT INTO `ip_hits` (`record_id`, `user_agent`, `session_id`, `hits`, `ip_address`, `access_time`) VALUES
-(2, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36', 'dl4nu95o6i9j1v02rlfjt206i1', 16, '::1', '2018-01-01 17:25:21'),
-(3, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393', '6jp719j94ltdatbjf89isr9t77', 2, '::1', '2018-01-01 17:00:02');
+INSERT INTO `ip_hits` (`customer_id`, `user_agent`, `session_id`, `hits`, `ip_address`, `access_time`) VALUES
+(1, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', 'tijgjt32r142eib5e84fp6noac', 4, '::1', '2019-06-06 15:17:06');
 
 -- --------------------------------------------------------
 
@@ -246,19 +70,17 @@ INSERT INTO `ip_hits` (`record_id`, `user_agent`, `session_id`, `hits`, `ip_addr
 
 CREATE TABLE `languages` (
   `language_id` int(11) NOT NULL,
-  `language` varchar(50) DEFAULT NULL,
-  `language_code` varchar(5) NOT NULL,
-  `is_active` tinyint(3) UNSIGNED NOT NULL DEFAULT '1'
+  `language` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `languages`
 --
 
-INSERT INTO `languages` (`language_id`, `language`, `language_code`, `is_active`) VALUES
-(1, 'English', 'en', 1),
-(2, 'తెలుగు', 'te', 1),
-(3, 'हिन्दी', 'hi', 0);
+INSERT INTO `languages` (`language_id`, `language`) VALUES
+(1, 'English'),
+(2, 'తెలుగు'),
+(3, 'हिन्दी');
 
 -- --------------------------------------------------------
 
@@ -291,21 +113,24 @@ INSERT INTO `messages` (`message_id`, `language_id`, `message_key`, `message`) V
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sessions`
+-- Table structure for table `test`
 --
 
-CREATE TABLE `sessions` (
-  `session_id` varchar(100) NOT NULL,
-  `session_data` text NOT NULL,
-  `session_lastaccesstime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `test` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `sessions`
+-- Dumping data for table `test`
 --
 
-INSERT INTO `sessions` (`session_id`, `session_data`, `session_lastaccesstime`) VALUES
-('6jp719j94ltdatbjf89isr9t77', 'user_id|s:1:\"1\";cart|O:11:\"Models\\cart\":1:{s:21:\"\0Models\\cart\0products\";a:3:{i:2;s:1:\"2\";i:4;s:2:\"61\";i:5;s:2:\"65\";}}', '2018-01-02 13:59:33');
+INSERT INTO `test` (`id`, `name`, `description`, `updated_at`) VALUES
+(1, 'test1', 'test test', '2019-06-03 13:44:27'),
+(2, 'test2', 'test2 test2', '2019-06-03 14:41:14'),
+(3, 'test3', 'test3 test3', '2019-06-03 14:42:18');
 
 -- --------------------------------------------------------
 
@@ -339,21 +164,11 @@ ALTER TABLE `cart_products`
   ADD PRIMARY KEY (`product_id`);
 
 --
--- Indexes for table `currencies`
---
-ALTER TABLE `currencies`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `code` (`code`),
-  ADD UNIQUE KEY `description` (`description`);
-
---
 -- Indexes for table `ip_hits`
 --
 ALTER TABLE `ip_hits`
-  ADD PRIMARY KEY (`record_id`,`session_id`,`ip_address`) USING BTREE,
-  ADD UNIQUE KEY `session_id` (`session_id`),
-  ADD UNIQUE KEY `Ip_session` (`session_id`,`ip_address`),
-  ADD UNIQUE KEY `record_id` (`record_id`);
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `session_id` (`session_id`);
 
 --
 -- Indexes for table `languages`
@@ -369,11 +184,10 @@ ALTER TABLE `messages`
   ADD UNIQUE KEY `Unique_messsage` (`language_id`,`message_key`);
 
 --
--- Indexes for table `sessions`
+-- Indexes for table `test`
 --
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`session_id`),
-  ADD UNIQUE KEY `session_id` (`session_id`);
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -391,17 +205,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart_products`
 --
 ALTER TABLE `cart_products`
-  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `currencies`
---
-ALTER TABLE `currencies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `ip_hits`
 --
 ALTER TABLE `ip_hits`
-  MODIFY `record_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `languages`
 --
@@ -412,6 +221,11 @@ ALTER TABLE `languages`
 --
 ALTER TABLE `messages`
   MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -426,49 +240,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `languages` (`language_id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
---
--- Table structure for table `test`
---
-
-CREATE TABLE `test` (
-  `id` int(10)  NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `test`
---
-
-INSERT INTO `test` (`id`, `name`, `description`, `updated_at`) VALUES
-(0000000001, 'test', 'test', '2019-06-01 18:52:46');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `test`
---
-ALTER TABLE `test`
-  MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
